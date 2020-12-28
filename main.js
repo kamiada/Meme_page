8f27ba90073402a7a8e3105be96a2f4a5d59dcd4
kamiada <kamiada528@gmail.com>
2020-12-28 15:10:02 +0000

    making things a bit more complex

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
 //http://apimeme.com/meme?meme=Advice-Doge&top=Much+sense&bottom=Much+love
 
-// fetch('https://dog.ceo/api/breed/bulldog/images/random/50')
-fetch('http://alpha-meme-maker.herokuapp.com/1')
-.then(response => response.json())
-.then(data=> data.message.map(pic => {
-    const body = document.querySelector('body');
-    const image = document.createElement('img');
-    image.src = pic;
-    body.append(image)
-    console.log(pic)
-}))
-.catch(error => console.error(error))
+//on click event - depending to what user picked from the menu, load this on the page
+const apiDogs = 'https://dog.ceo/api/breed/bulldog/images/random/50';
+const apiMemes = 'http://alpha-meme-maker.herokuapp.com/1';
+
+function whatToLoad (menuItem) {
+    const pictures = document.querySelector('img');
+    if(typeof(pictures) != 'undefined' && pictures !=null){
+        location.reload(); 
+    }
+    if(menuItem === 'Dogs')
+    {
+        return fetch(apiDogs)
+        .then(response => response.json())
+        .then(data=> data.message.map(pic => {
+            const body = document.querySelector('body');
+            const image = document.createElement('img');
+            image.src = pic;
+            body.append(image)
+            console.log(pic)
+        }))
+        .catch(error => console.error(error));
+    }
+    if(menuItem === 'Memes'){
+        return fetch(apiMemes)
+        .then(response => response.json())
+        .then(data => data.message.map(pic => {
+            const body = document.querySelector('body');
+            const image = document.createElement('img');
+            image.src = pic;
+            body.append(image)
+            console.log(pic)
+        }))
+        .catch(error => console.error(error));
+    }
+    return fetch(apiMemes)
+    .then(response => response.json())
+    .then(data=> data.message.map(pic => {
+        const body = document.querySelector('body');
+        const image = document.createElement('img');
+        image.src = pic;
+        body.append(image)
+        console.log(pic)
+    }))
+    .catch(error => console.error(error));
+}
